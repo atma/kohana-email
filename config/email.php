8ebfbfd1412ba77f8cf62a8df83c1d2a98c337b3
Oleh Burkhay <oleg.burhay@gmail.com>
2011-11-29 15:48:05 -0300

    swiftmailer updated to v4.1.3; kohana v3.2 compatible

--- config/email.php
+++ config/email.php
@@ -11,13 +11,6 @@ return array(
 	 * To use secure connections with SMTP, set "port" to 465 instead of 25.
 	 * To enable TLS, set "encryption" to "tls".
 	 * 
-	 * Note for SMTP, 'auth' key no longer exists as it did in 2.3.x helper
-	 * Simply specifying a username and password is enough for all normal auth methods
-	 * as they are autodeteccted in Swiftmailer 4
-	 * 
-	 * PopB4Smtp is not supported in this module as I had no way to test it but 
-	 * SwiftMailer 4 does have a PopBeforeSMTP plugin so it shouldn't be hard to implement
-	 * 
 	 * Encryption can be one of 'ssl' or 'tls' (both require non-default PHP extensions
 	 *
 	 * Driver options:
